d388ed71bfc1b156e455dcff5b806e7117492bad
Berxz <37065724+Berxz@users.noreply.github.com>
2020-04-16 01:10:23 +0800

    Update index.php

--- PROJECT/index.php
+++ PROJECT/index.php
@@ -103,25 +103,28 @@
 <div align="center" class="w3-content w3-display-container">
 
 <div class="w3-display-container mySlides">
-  <img src="party.jpg" style="width:45%">
   <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
-    Party
-  </div>
+    Party Theme
+  </div><br>
+  <img src="party.jpg" style="width:40%">
+
 </div>
 
 <div class="w3-display-container mySlides">
-  <img src="anniv.jpg" style="width:45%">
   <div class="w3-display-bottomright w3-large w3-container w3-padding-16 w3-black">
-    Anniversary
-  </div>
+    Anniversary Theme
+  </div><br>
+  <img src="anniv.jpg"  style="width:40%">
+
 </div>
 
 <div class="w3-display-container mySlides">
-  <img src="wedding.jpg" style="width:45%">
   <div class="w3-display-topleft w3-large w3-container w3-padding-16 w3-black">
-    Wedding
-  </div>
-</div>
+    Wedding Theme
+  </div><br>
+  <img src="wedding.jpg" style="width:40%">
+
+</div><br>
 
 <button  class="w3-button w3-display-left w3-black" onclick="plusDivs(-1)">&#10094;</button>
 <button class="w3-button w3-display-right w3-black" onclick="plusDivs(1)">&#10095;</button>
